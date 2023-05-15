4cde91165f0ec9f570531d37542e3a3ea4bb4650
Danila Legkobytov <bjorn-bis@yandex.ru>
2023-05-15 21:20:40 +0700

    bugfix: Исправить путь в HashRouter

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -14,7 +14,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <HelmetProvider>
-      <HashRouter basename='/'>
+      <HashRouter>
         <App />
       </HashRouter>
     </HelmetProvider>
